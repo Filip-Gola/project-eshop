b71042f480bf40b039e93c6b506dd03fb83f7183
Filip-Gola <fgola40@gmail.com>
2019-08-25 22:33:48 +0200

    vymazal som stary js kod ktory nebol pouzity

--- js/index.js
+++ js/index.js
@@ -1,46 +1,8 @@
-// zobrazovanie elementov a blokov HTML
+'use strict'
 
 $(document).ready(function () {
 	
-	// Jquery pre navigaciu
-	// na zaciatku skryjem doplnkove bloky menu a bude si ich volat na hover alebo click;
-	$('#hover-menu').hide();
-	$('#mobile-menu').hide();
-
-	let width = $(window).width();
-	//sirka pre male obrazovky
-	if (width <= 770) {
-		
-		// klik na hamburger menu, porovna ci je #mobile-menu zobrazene alebo nie. Ak neni zobraz ho...
-		$('#btn-mobile-menu').click(function () {
-			if($('#mobile-menu').css('display') === 'none'){
-				$('#mobile-menu').fadeIn();
-				$('.fa-bars').removeClass('fa-bars').addClass('fa-times');
-			}else{
-				$('#mobile-menu').fadeOut();
-				$('.fa-times').removeClass('fa-times').addClass('fa-bars');
-			}
-		}); 
-	} else {
-		// velke obrazovky
-		$('#hover-menu-btn').hover(function () {
-			$('#hover-menu').fadeIn("slow");
-		})
-		$('#hover-menu').mouseleave(function () {
-			$('#hover-menu').fadeOut();
-		})
-	}
-
-	// jquery pre zobrazenie vyhladavaca velke obrazovky
-	$('#search-btn').click(function(){
-		if($('#search-block').css('display') == 'none'){
-			$('#search-block').fadeIn();
-			$('.fa-search').removeClass('fa-search').addClass('fa-times');
-		}else{
-			$('#search-block').fadeOut();
-			$('.fa-times').removeClass('fa-times').addClass('fa-search');
-		}
-	})	
+	
 });
 
 
