8069d6c19bdf08104193ebab18fd6e45e62852fb
Filip-Gola <fgola40@gmail.com>
2019-08-25 23:29:13 +0200

    generovanie dat z json pre produkt.html

--- js/index.js
+++ js/index.js
@@ -2,7 +2,38 @@
 
 $(document).ready(function () {
 	
-	
+	$('.product-info').click(getProductId);
+
+	$.get("js/products.json", function (products) {
+		renderProductImages(products);
+		renderProductInfo(products);
+	});
 });
 
 
+// zisti product id
+function getProductId() {
+	let productID = $(this).parent().parent().attr('data-id');
+	localStorage.setItem('product-id', productID);
+};
+
+function renderProductImages(products) {
+	let productID = localStorage.getItem('product-id');
+	let product = '';
+	product += `<img src=${JSON.stringify(products[productID - 1].url[0])} alt=${JSON.stringify(products[productID - 1].name)} class="product-img"/>`;
+	product += `<img src=${JSON.stringify(products[productID - 1].url[1])} alt=${JSON.stringify(products[productID - 1].name)} class="product-img"/>`;
+
+	$('.product-imgs').append(product);
+};
+
+function renderProductInfo(products) {
+	let productID = localStorage.getItem('product-id');
+	let product = '';
+	product += `<h1>${products[productID-1].name.toUpperCase()}</h1>`;
+	product += `<p>${products[productID-1].description}</p>`;
+	product += `<p>${products[productID-1].description}</p>`;
+	product += `<p>${products[productID-1].description}</p>`;
+	product += `<h2>${products[productID-1].price}€</h2>`;
+
+	$('#product-info').prepend(product);
+}
